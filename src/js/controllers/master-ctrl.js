a04c4248e7aa7b67b9c9c2821061f7e2d79bc112
jota <juanbanelli@gmail.com>
2016-05-15 11:52:14 -0300

    Side navbar uncollapsed by default

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -1,29 +1,31 @@
-angular.module('RDash').controller('MasterCtrl', ['$scope', '$cookieStore', function($scope) {
+angular.module('RDash').controller('MasterCtrl', ['$scope', '$cookieStore', 'apiRequest', function($scope, $cookieStore, apiRequest) {
   var mobileView = 992;
+  $scope.toggle = false;
+
   $scope.getWidth = function() {
       return window.innerWidth;
   };
 
   $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-      if (newValue >= mobileView) {
-          if (angular.isDefined($cookieStore.get('toggle'))) {
-              $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
-          } else {
-              $scope.toggle = true;
-          }
+    if (newValue >= mobileView) {
+      if (angular.isDefined($cookieStore.get('toggle'))) {
+        $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
       } else {
-          $scope.toggle = false;
+        $scope.toggle = true;
       }
+    } else {
+      $scope.toggle = false;
+    }
 
   });
 
   $scope.toggleSidebar = function() {
-      $scope.toggle = !$scope.toggle;
-      $cookieStore.put('toggle', $scope.toggle);
+    $scope.toggle = !$scope.toggle;
+    $cookieStore.put('toggle', $scope.toggle);
   };
 
   window.onresize = function() {
-      $scope.$apply();
+    $scope.$apply();
   };
 
 }]);
