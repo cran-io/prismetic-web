554fc39cd2e6b11ba9eb3b2b60d2ef7abf8d3f3d
jota <juanbanelli@gmail.com>
2016-05-15 10:52:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+##PRISMETIC WEB APP
+=========
+Web application based in [Rdash](http://rdash.github.io/). Customized by CRAN.IO
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    connect    = require('gulp-connect'),
+    watch      = require('gulp-watch'),
+    minifyCss  = require('gulp-cssnano'),
+    minifyJs   = require('gulp-uglify'),
+    concat     = require('gulp-concat'),
+    rename     = require('gulp-rename'),
+    minifyHTML = require('gulp-htmlmin');
+    wiredep    = require('wiredep').stream;
+    inject     = require('gulp-inject');
+    browSync   = require('browser-sync');
+
+var files = {
+    angularMain: 'src/js/*.js',
+    angularDirectives: 'src/js/directives/*.js',
+    angularControllers: 'src/js/controllers/*.js',
+    styles: 'src/css/*.css',
+    images: 'src/img/**',
+    templates: 'src/templates/*.html',
+    index: 'src/index.html',
+    bowerFonts: 'src/components/font-awesome/**/*.*',
+    bowerComponents: 'src/components/**/*.*'
+};
+
+gulp.task('inject-libs', function() {
+  gulp.src(files.index)
+    .pipe(inject(gulp.src([files.angularMain, files.angularDirectives, files.angularControllers, files.styles, files.bowerFonts], { read: false }), { relative: true }))
+    .pipe(wiredep())
+    .pipe(gulp.dest('src'));
+});
+
+gulp.task('browser-sync', ['inject-libs'], function() {
+  browSync.init([
+    files.angularMain, 
+    files.angularDirectives, 
+    files.angularControllers, 
+    files.styles,
+    files.templates,
+    files.index
+  ], {
+    server: {
+      baseDir: 'src'
+    }
+  }, function() {
+    console.log('up');
+  });
+
+});
+
+gulp.task('start', ['browser-sync']);
