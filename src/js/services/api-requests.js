0a4c6b2ea944957e4c1f9d528a1e7afa82d1cbc3
jota <juanbanelli@gmail.com>
2016-05-16 17:36:20 -0300

    Managing devices & sensors api requests

--- src/js/services/api-requests.js
+++ src/js/services/api-requests.js
@@ -1,11 +1,25 @@
 angular.module('RDash').factory('apiRequest', ['$http', '$q', function($http, $q) {
-  var apiUrl = 'http://prismetic.cran.io';
+  var apiUrl = 'http://192.168.1.56:8080/api';
   return {
     getDevices: function() {
       var deferred = $q.defer();
       $http({
         method: 'GET',
         url: apiUrl + '/devices',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }).success(function(response) {
+        deferred.resolve(response);
+      });
+      return deferred.promise;
+    }, 
+    
+    getSensors: function(deviceID) {
+      var deferred = $q.defer();
+      $http({
+        method: 'GET',
+        url: apiUrl + '/devices/' + deviceID + '/sensors',
         headers: {
           'Content-type': 'application/json'
         }
