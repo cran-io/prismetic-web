27b79e04db22419fa1e37585ba6dac0b76d196a6
jota <juanbanelli@gmail.com>
2016-05-18 21:58:52 -0300

    Started gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 var gulp = require('gulp'),
-    connect    = require('gulp-connect'),
     watch      = require('gulp-watch'),
     minifyCss  = require('gulp-cssnano'),
     minifyJs   = require('gulp-uglify'),
+    minifyImgs = require('gulp-imagemin'),
+    lib        = require('bower-files')(),
     concat     = require('gulp-concat'),
-    rename     = require('gulp-rename'),
-    minifyHTML = require('gulp-htmlmin');
-    wiredep    = require('wiredep').stream;
-    inject     = require('gulp-inject');
+    minifyHTML = require('gulp-htmlmin'),
+    wiredep    = require('wiredep').stream,
+    inject     = require('gulp-inject'),
     browSync   = require('browser-sync');
-
+    
 var files = {
     angularMain: 'src/js/*.js',
     angularDirectives: 'src/js/directives/*.js',
     angularControllers: 'src/js/controllers/*.js',
     angularServices: 'src/js/services/*.js',
     styles: 'src/css/*.css',
-    images: 'src/img/**',
+    images: 'src/img/*.*',
+    fonts: 'src/fonts/*.*',
     templates: 'src/templates/*.html',
     index: 'src/index.html',
     bowerFonts: 'src/components/font-awesome/**/*.*',
@@ -24,10 +25,67 @@ var files = {
 };
 
 gulp.task('inject-libs', function() {
-  gulp.src(files.index)
+  return gulp.src(files.index)
     .pipe(inject(gulp.src([files.angularMain, files.angularServices, files.angularDirectives, files.angularControllers, files.styles, files.bowerFonts], { read: false }), { relative: true }))
     .pipe(wiredep())
     .pipe(gulp.dest('src'));
+});
+
+gulp.task('set-index', function() {
+  return gulp.src(files.index)
+    .pipe(inject(gulp.src(['dist/css/*.css', 'dist/js/*.js'], { read: false }), { relative: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-templates', function() {
+  return gulp.src(files.templates)
+    .pipe(minifyHTML({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/templates'));
+});
+
+gulp.task('minify-index', function() {
+  return gulp.src(files.index)
+   .pipe(minifyHTML({ collapseWhitespace: true }))
+   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css-custom', function() {
+  return gulp.src(files.styles)
+    .pipe(concat('all.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js-custom', function() {
+  return gulp.src([files.angularMain, files.angularServices, files.angularDirectives, files.angularControllers])
+    .pipe(concat('all.min.js'))
+    .pipe(minifyJs())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-libs', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('lib.min.js'))
+    .pipe(minifyJs())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css-libs', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('lib.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('fonts-custom', function() {
+  return gulp.src(files.fonts)
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', function() {
+  return gulp.src(files.images)
+    .pipe(minifyImgs())
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('browser-sync', ['inject-libs'], function() {
@@ -50,3 +108,4 @@ gulp.task('browser-sync', ['inject-libs'], function() {
 });
 
 gulp.task('start', ['browser-sync']);
+gulp.task('build', ['js-libs', 'css-libs', 'css-custom', 'js-custom', 'fonts-custom', 'images', 'set-index', 'minify-templates']);
