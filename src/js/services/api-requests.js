d5c66639f57cc76887cc1b4e58adf5a09c864f6c
jota <juanbanelli@gmail.com>
2016-05-26 17:08:51 -0300

    Added route to get device data

--- src/js/services/api-requests.js
+++ src/js/services/api-requests.js
@@ -29,6 +29,22 @@ angular.module('Prismetic').factory('apiRequest', ['$http', '$q', function($http
       return deferred.promise;
     },
 
+    getDeviceData: function(deviceID, params) {
+      var deferred = $q.defer();
+      $http({
+        method: 'GET',
+        params: params,
+        url: apiUrl + '/devices/' + deviceID + '/graphSensorData',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }).success(function(response) {
+        deferred.resolve(response);
+      });
+      return deferred.promise;
+      
+    },
+
     getSensorData: function(deviceID, sensorID, params) {
       var deferred = $q.defer();
       $http({
