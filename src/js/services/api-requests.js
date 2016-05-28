94c524e366c140a9da07e890ef834a8e2dff588e
jota <juanbanelli@gmail.com>
2016-05-28 13:21:21 -0300

    Preparing API requests to update device attributes

--- src/js/services/api-requests.js
+++ src/js/services/api-requests.js
@@ -1,56 +1,87 @@
 angular.module('Prismetic').factory('apiRequest', ['$http', '$q', function($http, $q) {
-  var apiUrl = 'http://prismetic.cran.io:8080/api';
-  return {
-    getDevices: function() {
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: apiUrl + '/devices',
-        headers: {
-          'Content-type': 'application/json'
-        }
-      }).success(function(response) {
-        deferred.resolve(response);
-      });
-      return deferred.promise;
-    }, 
-    
-    getSensors: function(deviceID) {
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: apiUrl + '/devices/' + deviceID + '/sensors',
-        headers: {
-          'Content-type': 'application/json'
-        }
-      }).success(function(response) {
-        deferred.resolve(response);
-      });
-      return deferred.promise;
-    },
+  var apiUrl   = 'http://prismetic.cran.io:8080/api';
+  var requests = {};
 
-    getDeviceData: function(deviceID, params) {
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        params: params,
-        url: apiUrl + '/devices/' + deviceID + '/graphSensorData',
-        headers: {
-          'Content-type': 'application/json'
-        }
-      }).success(function(response) {
-        deferred.resolve(response);
-      });
-      return deferred.promise;
-      
-    },
+  requests.getDevices =  function() {
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      url: apiUrl + '/devices',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).success(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+  
+  requests.getSensors = function(deviceID) {
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      url: apiUrl + '/devices/' + deviceID + '/sensors',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).success(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
 
-    getSensorData: function(deviceID, sensorID, params) {
+  requests.getDeviceData = function(deviceID, params) {
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      params: params,
+      url: apiUrl + '/devices/' + deviceID + '/graphSensorData',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).success(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  requests.getSensorData = function(deviceID, sensorID, params) {
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      params: params,
+      url: apiUrl + '/devices/' + deviceID + '/sensors/' + sensorID + '/average',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).success(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  requests.postDeviceName = function(deviceID, params) {
+    var deferred = $q.defer();
+    $http({
+      method: 'PUT',
+      params: params,
+      url: apiUrl + '/devices/' + deviceID,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).success(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  ['postDeviceName', 'postAddressLocation'].forEach(function(post) {
+    requests[post] = function(deviceID, params) {
       var deferred = $q.defer();
       $http({
-        method: 'GET',
+        method: 'PUT',
         params: params,
-        url: apiUrl + '/devices/' + deviceID + '/sensors/' + sensorID + '/average',
+        url: apiUrl + '/devices/' + deviceID,
         headers: {
           'Content-type': 'application/json'
         }
@@ -58,6 +89,10 @@ angular.module('Prismetic').factory('apiRequest', ['$http', '$q', function($http
         deferred.resolve(response);
       });
       return deferred.promise;
-    }
-  }
+    };
+
+  });
+
+  
+  return requests;
 }]);
