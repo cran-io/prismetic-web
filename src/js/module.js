db7d3a53e2a0604f477b325860696a6180ef62d9
jota <juanbanelli@gmail.com>
2016-05-29 14:21:54 -0300

    Added animations

--- src/js/module.js
+++ src/js/module.js
@@ -1,5 +1,5 @@
 angular
-  .module('Prismetic', ['ui.bootstrap', 'ui.router', 'ngCookies', 'btford.socket-io', 'xeditable'])
+  .module('Prismetic', ['ui.bootstrap', 'ui.router', 'ngCookies', 'btford.socket-io', 'xeditable', 'ngAnimate'])
   .run(['editableOptions', function(editableOptions) {
     editableOptions.theme = 'bs3';
   }]);
