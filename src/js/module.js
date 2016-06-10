d44b8354859b8f5fd76736f2cd176a71e5249639
Gonzalo Pombo <gonpombo@gmail.com>
2016-06-10 15:36:09 -0300

    Testing provider

--- src/js/module.js
+++ src/js/module.js
@@ -2,18 +2,18 @@ angular
   .module('Prismetic', ['ui.bootstrap', 'ui.router', 'ngCookies', 'btford.socket-io', 'xeditable', 'ngAnimate', 'ngStorage'])
   .config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.withCredentials = true;
-    $httpProvider.interceptors.push(function($q, $location, $localStorage) {
-      return {
-        responseError: function(response) {
-          if (response.status === 401) {
-            delete $localStorage.user;
-            console.log("C", $localStorage)
-            $location.path('/login');
-          }
-          return $q.reject(response);
-        }
-      };
-    });
+    // $httpProvider.interceptors.push(function($q, $location, $localStorage) {
+    //   return {
+    //     responseError: function(response) {
+    //       if (response.status === 401) {
+    //         delete $localStorage.user;
+    //         console.log("C", $localStorage)
+    //         $location.path('/login');
+    //       }
+    //       return $q.reject(response);
+    //     }
+    //   };
+    // });
   }])
   .run(['editableOptions', function(editableOptions) {
     editableOptions.theme = 'bs3';
