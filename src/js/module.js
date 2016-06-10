ac6533c5e9a1d13b7fec9d86177b3b0f69eab6a8
Gonzalo Pombo <gonpombo@gmail.com>
2016-06-10 15:41:14 -0300

    Interceptor HTTP to factory

--- src/js/module.js
+++ src/js/module.js
@@ -1,19 +1,20 @@
 angular
   .module('Prismetic', ['ui.bootstrap', 'ui.router', 'ngCookies', 'btford.socket-io', 'xeditable', 'ngAnimate', 'ngStorage'])
+  .factory('httpInterceptor', ['$q', '$location', '$localStorage', function() {
+      return {
+        responseError: function(response) {
+          if (response.status === 401) {
+            delete $localStorage.user;
+            console.log("C", $localStorage)
+            $location.path('/login');
+          }
+          return $q.reject(response);
+        }
+      };
+  }])
   .config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.withCredentials = true;
-    // $httpProvider.interceptors.push(function($q, $location, $localStorage) {
-    //   return {
-    //     responseError: function(response) {
-    //       if (response.status === 401) {
-    //         delete $localStorage.user;
-    //         console.log("C", $localStorage)
-    //         $location.path('/login');
-    //       }
-    //       return $q.reject(response);
-    //     }
-    //   };
-    // });
+    $httpProvider.interceptors.push('httpInterceptor');
   }])
   .run(['editableOptions', function(editableOptions) {
     editableOptions.theme = 'bs3';
